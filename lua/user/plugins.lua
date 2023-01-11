d0decb906146e1d3cad0b2d0ccbc15f5090ba663
Norel <norel.evoagile@gmail.com>
2023-01-12 01:10:33 +0900

    Remove addtional colorscheme definition

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -52,10 +52,6 @@ return packer.startup(function(use)
   -- Colorschemes
   use {"lunarvim/colorschemes", 'folke/tokyonight.nvim'}
 
-  -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
-
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
