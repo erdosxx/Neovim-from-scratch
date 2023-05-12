3cad70eed07e5ec2ca1c220b6fe7e3ca46b4cfae
Norel <norel.evoagile@gmail.com>
2023-05-12 15:12:26 +0900

    Add comments to setup Julia Language setup with Ref url and uncomment echo message for debugging.

--- lua/user/lsp/settings/julials.lua
+++ lua/user/lsp/settings/julials.lua
@@ -1,10 +1,14 @@
+-- To use this at the first time, run following command
+-- julia --project=~/.julia/environments/nvim-lspconfig
+-- -e 'using Pkg; Pkg.add("LanguageServer")'
+-- Ref: https://discourse.julialang.org/t/neovim-languageserver-jl/37286/63?page=5
 return {
 	on_new_config = function(new_config, _)
 		local julia =
 			vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
-		vim.notify("Before Hello")
+		--[[ vim.notify("Before Hello") ]]
 		if require("lspconfig").util.path.is_file(julia) then
-			vim.notify("Hello!")
+			--[[ vim.notify("Hello!") ]]
 			new_config.cmd[1] = julia
 		end
 	end,
