267a28e7b068f0b9778dbea2548987842bff875f
Norel <norel.evoagile@gmail.com>
2023-05-20 12:46:43 +0900

    Update and add keymaps for cmp.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,4 +1,6 @@
 local opts = { noremap = true, silent = true }
+local opts_remap = { noremap = false, silent = true }
+local opts_remap_echo = { noremap = false, silent = false }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
@@ -155,6 +157,11 @@ keymap("v", "<localleader>j", ":JuliaFormatterFormat<CR>", opts)
 keymap(
 	"n",
 	"<localleader>/",
-	"V{:JuliaFormatterFormat<CR>}V{:JuliaREPLSend<CR>}",
-	opts
+	"V%:JuliaFormatterFormat<CR>%V%:JuliaREPLSend<CR>%",
+	opts_remap -- for using %, need to use remap
 )
+-- when cmp give choices,
+-- abort it and move forward to next input for snippet.
+keymap("i", "<F2>", "<ESC>i<C-j>", opts_remap_echo)
+-- remove comment string and add normal input for coding.
+keymap("i", "<F3>", "<ESC>gcci", opts_remap_echo)
